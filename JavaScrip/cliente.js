5d39539ce91682179f8cacb6e0944437696cbbe2
IT <lizeth.avila@unah.hn>
2025-04-03 14:57:33 -0600

    Funcionalidades de estilos y modales

--- JavaScrip/cliente.js
+++ JavaScrip/cliente.js
@@ -37,3 +37,73 @@ class ClienteApp {
             this.confirmarEliminacion($(e.currentTarget).data('id'));
         });
     }
+
+
+    bindEvents() {
+        $('#btnBuscar').click(() => this.buscarClientes());
+        $('#btnAgregarCliente').click(() => this.prepararFormulario());
+        $('#btnGuardar').click(() => this.guardarCliente());
+        $('#btnConfirmarEliminar').click(() => this.eliminarCliente());
+
+        this.$inputBusqueda.keypress((e) => {
+            if (e.which === 13) this.buscarClientes();
+        });
+
+        this.$tablaClientes.on('click', '.btn-editar', (e) => {
+            this.editarCliente($(e.currentTarget).data('id'));
+        });
+
+        this.$tablaClientes.on('click', '.btn-eliminar', (e) => {
+            this.confirmarEliminacion($(e.currentTarget).data('id'));
+        });
+    }
+
+    setupModalHandlers() {
+        // Manejar el cierre del modal de cliente
+        this.$modalCliente.on('hidden.bs.modal', () => {
+            this.mostrarLoading(false);
+            this.cargarClientes(); // Recargar lista al cancelar
+        });
+
+        // Manejar el cierre del modal de confirmación
+        this.$modalConfirmacion.on('hidden.bs.modal', () => {
+            this.mostrarLoading(false);
+            // No recargamos aquí para evitar doble carga
+        });
+    }
+
+    cargarClientes() {
+        this.mostrarLoading(true);
+        
+        $.ajax({
+            url: this.apiUrl,
+            method: 'GET',
+            dataType: 'json'
+        })
+        .done((data) => {
+            this.mostrarClientes(data);
+        })
+        .fail((error) => {
+            console.error('Error al cargar clientes:', error);
+            this.mostrarError('Error al cargar clientes', error);
+        })
+        .always(() => {
+            this.mostrarLoading(false);
+        });
+    }
+
+    mostrarLoading(mostrar) {
+        if (mostrar) {
+            this.$tablaClientes.html(`
+                <tr>
+                    <td colspan="8" class="text-center py-5">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Cargando...</span>
+                        </div>
+                    </td>
+                </tr>
+            `);
+        } else if (this.$tablaClientes.find('.spinner-border').length) {
+            this.$tablaClientes.empty();
+        }
+    }
